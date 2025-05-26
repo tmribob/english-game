45b22f8acad9ad89e6d4170ba2c0e22a08115e85
Tmribob <mmitko2141@gmail.com>
2025-05-26 23:33:40 +0300

    improve the principle of “split”: now “,” the beginning of a sentence can be anything and the fourth 4 has been added.

--- src/Hooks/UseAddText.js
+++ src/Hooks/UseAddText.js
@@ -31,8 +31,8 @@ const UseAddText = (navigate, showNotification) => {
         clearInputs();
     }
     const splitText = (text) => {
-        const sentences = text.split(/[.!?]\s*/).filter(sentence => sentence.trim());
-        return sentences.map(sentence => sentence.match(/[а-яА-ЯёЁa-zA-Z0-9]+(?:['-`][а-яА-ЯёЁa-zA-Z0-9]+ | ,)*/g));
+        const sentences = text.replace(/([,:—–-])/g, ` $1 `).split(/[.!?]\s*/).filter(sentence => (/[a-zA-Zа-яА-ЯёЁ]/).test(sentence.trim()));
+        return sentences.map(sentence => sentence.replace(/^[^a-zA-Zа-яА-ЯёЁ]*/, '').match(/([а-яА-ЯёЁa-zA-Z0-9]+(?:['-`][а-яА-ЯёЁa-zA-Z0-9]+)*|[,-:])/g));
     }
     const changeInputText = (e) => {
         setInputText(e.target.value);

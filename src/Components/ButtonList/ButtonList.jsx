3448e164c5d4b6ee17c018f831400160a5f2ff12
Tmribob <mmitko2141@gmail.com>
2025-06-08 16:48:06 +0300

    add li in lists

--- src/Components/ButtonList/ButtonList.jsx
+++ src/Components/ButtonList/ButtonList.jsx
@@ -3,14 +3,15 @@ import Button from "../Button/Button";
 
 const ButtonList = ({array, changeButton}) => {
   return (<ul className={style.buttonList}>
-    {array && array.map((value) => (<Button
-      addClass={value.isActive ? style.isActive : ""}
-      key={value.key}
-      width={"auto"}
-      theme={"blue"}
-      onClick={() => changeButton(value.key)}
-      content={value.word}
-    />))}
+    {array && array.map((value) => (<li key={value.key}>
+      <Button
+        addClass={value.isActive ? style.isActive : ""}
+        width={"auto"}
+        theme={"blue"}
+        onClick={() => changeButton(value.key)}
+        content={value.word}
+      />
+    </li>))}
   </ul>);
 }
 export default ButtonList;
--- src/Components/SpanList/SpanList.jsx
+++ src/Components/SpanList/SpanList.jsx
@@ -3,12 +3,13 @@ import Span from "../Span/Span";
 
 const SpanList = ({array, delSpan}) => {
   return (<ul className={style.spanList}>
-    {array && array.map((value) => (<Span
-      key={value.key}
-      onClick={() => delSpan(value.key)}
-      isRight={"isRight" in value ? value.isRight : ''}
-      content={value.word}
-    />))}
+    {array && array.map((value) => (<li key={value.key}>
+      <Span
+        onClick={() => delSpan(value.key)}
+        isRight={"isRight" in value ? value.isRight : ''}
+        content={value.word}
+      />
+    </li>))}
   </ul>);
 }
 
